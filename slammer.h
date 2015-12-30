23e4defe0308ab5c246134d863b776386a90c668
Benoit Cote-Jodoin <benoit.cote.jodoin@gmail.com>
2015-12-29 19:54:41 -0500

    added cooldown before moving slammer's motor

--- slammer.h
+++ slammer.h
@@ -1,12 +1,9 @@
 #ifndef SLAMMER_H
 #define SLAMMER_H
 
-#define SLAMMER_IDLE 0
-#define SLAMMER_SLAMMING 1
-#define SLAMMER_RETRACTING 2
-
-#define SLAMMER_SLAMMING_SPEED 20
-#define SLAMMER_RETRACTING_SPEED 20
+#define SLAMMER_SLAMMING_SPEED 30
+#define SLAMMER_RETRACTING_SPEED 30
+#define SLAMMER_COOLDOWN 30
 #define DOWN_SWITCH A0
 #define UP_SWITCH A1
 
@@ -18,6 +15,7 @@ public:
     _up_flag = false;
     _down_flag = false;
     _ticks = 0;
+    _cooldown = 0;
   }
 
   void setup() {
@@ -31,6 +29,7 @@ public:
       Serial.println(activated ? "true" : "false");
 
       _ticks = 0;
+      _cooldown = SLAMMER_COOLDOWN;
     }
 
     _activated = activated;
@@ -49,25 +48,30 @@ public:
       _up_flag = true;
     }
 
-    if(_activated && !_down_flag) {
-      if(_ticks >= 300) {
-        _down_flag = true;
-      } else {
-        slam();
-        moving = true;
-        _up_flag = false;
+    if(_cooldown == 0) {
+      if(_activated && !_down_flag) {
+        if(_ticks >= 30) {
+          _down_flag = true;
+        } else {
+          slam();
+          moving = true;
+          _up_flag = false;
+        }
+      }
+
+      if(!_activated && !_up_flag) {
+        if(_ticks >= 30) {
+          _up_flag = true;
+        } else {
+          retract();
+          moving = true;
+          _down_flag = false;
+        }
       }
     }
 
-    if(!_activated && !_up_flag) {
-      if(_ticks >= 300) {
-         _up_flag = true;
-      } else {
-        retract();
-        moving = true;
-        _down_flag = false;
-      }
-
+    if(_cooldown > 0) {
+      _cooldown--;
     }
 
     if(!moving) {
@@ -92,6 +96,7 @@ private:
   bool _down_flag;
   bool _activated;
   int _ticks;
+  int _cooldown;
 
   Adafruit_DCMotor *_motor;
 };
